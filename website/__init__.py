6572d444eae72675a51273ee43d06b9f9276bbdd
GiacomoFregnan <giacomo.fregnan@edu.unito.it>
2026-04-14 18:08:14 +0200

    test sul deploy

--- website/__init__.py
+++ website/__init__.py
@@ -28,6 +28,7 @@ def create_app():
     app.config['MAIL_SERVER'] = 'smtp.gmail.com'
     app.config['MAIL_PORT'] = 587
     app.config['MAIL_USE_TLS'] = True
+    app.config['MAIL_USE_SSL'] = False  # Deve essere False se usi la 587
     app.config['MAIL_USERNAME'] = os.getenv('MAIL_USERNAME')
     app.config['MAIL_PASSWORD'] = os.getenv('MAIL_PASSWORD')
     app.config['MAIL_DEFAULT_SENDER'] = os.getenv('MAIL_USERNAME')
